d84db72cbc4b26c4dc7e1efa087b9f70c5d32c90
Mehul Panchal <mehulcodes007@icloud.com>
2020-06-08 21:49:03 +0530

    Update gitdork.sh

--- gitdork.sh
+++ gitdork.sh
@@ -1,13 +1,16 @@
-# This script takes first argument as output folder
+#!/bin/bash
 
 for i in `ls`
 do
 	cd $i
-
+	
+	# Finding Emails
 	git log -p | grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" | sort | uniq >> $1/email.txt
-
+	
+	# Finding Linked users
 	git log -p | grep Author: | sort | uniq >> $1/name.txt
 
+	# Finding URLs
 	git log -p | grep -E -o '\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]' | sort | uniq >> $1/link.txt
 
 	cd ..
